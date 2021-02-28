a491b24325750799154557c545a187805723c6b1
mdodovic <matija.dodovic@gmail.com>
2021-02-28 18:32:16 +0100

    test 2 sollution addded

--- collections/Deque.java
+++ collections/Deque.java
@@ -156,18 +156,18 @@ public class Deque<Item> implements Iterable<Item> {
         myDeque.addFirst(13);
         myDeque.addLast(5);
 
-        System.out.println("Size: " + myDeque.size());
+        // System.out.println("Size: " + myDeque.size());
 
         Iterator<Integer> it = myDeque.iterator();
         
         while(it.hasNext()) {
-            System.out.println(it.next());
+            it.next();
         }
         myDeque.removeLast();
         myDeque.removeFirst();
         myDeque.removeFirst();
         
-        System.out.println("Size: " + myDeque.size());
+        // System.out.println("Size: " + myDeque.size());
     }
 
 }
--- collections/RandomizedQueue.java
+++ collections/RandomizedQueue.java
@@ -1,45 +1,157 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-    // construct an empty randomized queue
+    private Item[] items;
+    private int size = 0;
+
+    @SuppressWarnings("unchecked")
     public RandomizedQueue() {
-        
+        items = (Item[]) new Object[1];
     }
 
-    // is the randomized queue empty?
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
-    // return the number of items on the randomized queue
     public int size() {
-        return 0;
+        return size;
     }
 
-    // add the item
+    private boolean isStorageFull() {
+        return items.length == size;
+    }
+
+    private void halveStorage() {
+        resizeStorage(items.length / 2);
+    }
+
+    private void doubleStorage() {
+        resizeStorage(items.length * 2);
+    }
+
+    private boolean isStorageOversized() {
+        return items.length > 1 && size <= items.length / 4;
+    }
+    
+    @SuppressWarnings("unchecked")
+    private void resizeStorage(int newSize) {
+        Item[] newItemStorage = (Item[]) new Object[newSize];
+        for (int i = 0; i < size; i++) {
+            newItemStorage[i] = items[i];
+        }
+        items = newItemStorage;
+    }
+
+    
     public void enqueue(Item item) {
-        
+        if (item == null) {
+            throw new NullPointerException("Cannot add null-pointer to queue.");
+        }
+
+        if (isStorageFull()) {
+            doubleStorage();
+        }
+
+        items[size++] = item;
     }
 
-    // remove and return a random item
-    public Item dequeue() {
-        return null;
+    public Item dequeue() {  // remove and return a random item
+        if (size == 0) {
+            throw new NoSuchElementException("Cannot remove an item from an empty queue.");
+        }
+
+        int indexOfItemToReturn = StdRandom.uniform(size);
+        Item returnValue = items[indexOfItemToReturn];
+        size--;
+        items[indexOfItemToReturn] = items[size];
+        items[size] = null;
+
+        if (isStorageOversized()) {
+            halveStorage();
+        }
+
+        return returnValue;
     }
 
-    // return a random item (but do not remove it)
     public Item sample() {
-        return null;
+        if (size == 0) {
+            throw new NoSuchElementException("Cannot fetch an item from an empty queue.");
+        }
+
+        int indexOfItemToReturn = StdRandom.uniform(size);
+        return items[indexOfItemToReturn];
     }
 
-    // return an independent iterator over items in random order
+    @Override
     public Iterator<Item> iterator() {
-        return null;
+        return new RandomIterator();
     }
 
-    // unit testing (required)
+
+    private class RandomIterator implements Iterator<Item> {
+
+        private Item[] iteratorItems;
+        private int index;
+
+        public RandomIterator() {
+            iteratorItems = copyRandomQueueItems();
+            StdRandom.shuffle(iteratorItems);
+        }
+
+        @SuppressWarnings("unchecked")
+        private Item[] copyRandomQueueItems() {
+            Item[] copiedItems = (Item[]) new Object[size];
+            for (int i = 0; i < size; i++) {
+                copiedItems[i] = items[i];
+            }
+            return copiedItems;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index < iteratorItems.length;
+        }
+
+        @Override
+        public Item next() {
+            if (hasNext()) {
+                return iteratorItems[index++];
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Operation remove is not supported");
+        }
+    }
+    
+    
     public static void main(String[] args) {
+        RandomizedQueue<Integer> myRQ = new RandomizedQueue<Integer>();
         
+        myRQ.enqueue(10);
+        myRQ.enqueue(13);
+        myRQ.enqueue(5);
+
+        // System.out.println("Size: " + myRQ.size());
+
+        Iterator<Integer> it = myRQ.iterator();
+        
+        while(it.hasNext()) {
+            it.next();
+        }
+        
+        myRQ.dequeue();
+        myRQ.dequeue();
+        myRQ.dequeue();
+        
+        // System.out.println("Size: " + myRQ.size());
     }
 
 }
